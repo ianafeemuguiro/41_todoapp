0643bc84499955bd24fbab95d3059779d2273fc8
ianafeemuguiro <ianafeyyy@gmail.com>
2016-10-21 11:35:22 -0500

    Ejercicio twitter

--- 41_todoapp/js/main.js
+++ 41_todoapp/js/main.js
@@ -1,17 +1,52 @@
-function (){
+
+alert("Hola bebes");
+(function (){
 	var lista = document.getElementById ("lista"),
 		tareaInput = document.getElementById ("tareaInput"),
 		btnNuevo = document.getElementById ("agregar");
-}
 
-var agregarTarea = function(){};
-var comprobarInput = function(){};
-var eliminarTarea = function(){};
+	var agregarTarea = function (){
+		var tarea = tareaInput.value,
+			nuevaTarea = document.createElement("li"),
+			enlace = document.createElement("a");
+			contenido = document.createTextNode (tarea);
 
-btnNuevaTarea.addEventListener ("click, agregarTarea");
+		if (tarea ==""){
+			tareaInput.setAttribute ("placeholder", "Agregar tarea");
+			tareaInput.className ="error";
+			return false;
+		}
 
-tareaInput.addEventListener("click",comprobarInput);
+		enlace.appendChild (contenido);
+		enlace.setAttribute("href", "#");
+		nuevaTarea.appendChild(enlace);
+		lista.appendChild(nuevaTarea);
 
-for (var i = 0; i <= lista.children.length -1; i ++){
-	lista.children[i].addEventListener("click",eliminarTarea);
-}
+		tareaInput.value="";
+
+		for (var i = 0; i <= lista.children.length -1; i++) {
+		lista.children[i].addEventListener("click", function(){
+		this.parentNode.removeChild(this);
+			});
+
+		}
+
+	};
+
+	var verificarImput = function (){
+	tareaInput.className="";
+	tareaInput.setAttribute("placeholder","Agregar tarea");
+	};	
+
+	var eliminarTarea = function(){
+	this.parentNode.removeChild(this);
+	};
+
+	btnNuevo.addEventListener("click", agregarTarea);
+	tareaInput.addEventListener("click",verificarImput);
+
+	for (var i = 0; i <= Things.lista.children.length -1; i++) {
+		lista.children[i].addEventListener("click",borrarTarea);
+	}
+
+}());
